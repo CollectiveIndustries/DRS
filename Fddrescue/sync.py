2fa8abae0d65bcb900b884825d2d271040a9f4b1
Admiral-CW <admmorketh@gmail.com>
2017-01-16 11:28:20 -0700

    Removes DDrescue from sync file, adds in partition information from lsblk

--- Fddrescue/sync.py
+++ Fddrescue/sync.py
@@ -22,6 +22,8 @@ MediaLocation = '/media/data/'
 LocalMount = ['lowntfs-3g', '-o', 'windows_names,ignore_case']
 NasMount = ['mount.cifs', BackupServer, '-o', 'username=root,password=cw8400', MediaLocation]
 
+# get a list of block devices
+block_list = ['lsblk', '--json', '--noheadings', '-o', 'name,size,model,serial,fstype']
 Rsync = ['rsync', '--recursive', '--compress-level=9', '--human-readable', '--progress', '--list-only', '--exclude-from=/etc/rsync_exclude.conf']
 
 # This class provides the functionality we want. You only need to look at
@@ -69,8 +71,6 @@ while Question is None:
 	print color.BOLD+"\nAttached Storage Devices.\n"+color.END
 
 # Search for disks
-#        grep = Popen(['grep','Disk /dev'], stdin=PIPE, stdout=PIPE)
-#        fdisk = Popen('fdisk -l'.split(), stdout=grep.stdin)
 	lsblk = Popen(block_list, stdout=PIPE, stderr=PIPE)
 	out, err = lsblk.communicate()
 
@@ -86,124 +86,25 @@ while Question is None:
 				print color.HEADER+"Model:  "+color.END+x['model']
 			if x['serial'] is not None:
 				print color.HEADER+"Serial: "+color.END+x['serial']
+			for c in x['children']:
+				print "\t"+color.HEADER+"Partition: "+color.OKGREEN+"/dev/"+c['name']+color.END
+				if c['fstype'] is not None:
+					print "\t"+color.HEADER+"Partition Type: "+color.OKGREEN+c['fstype']+color.END
+				else:
+					print "\t"+color.HEADER+"Partition Type: "+color.FAIL+"UNKNOWN"+color.END
+				if c['size'] is not None:
+					print "\t"+color.HEADER+"Partition Size: "+color.OKGREEN+c['size']+color.END
+				else:
+					print"\t"+color.HEADER+"Partition Size: "+color.FAIL+"UNKNOWN"+color.END
+				print ""
 		print "" # add a blank line at the end of each group as some values may not print
 
 	except (ValueError, KeyError, TypeError):
 	    print "lsblk returned the wrong JSON format"
 
 # Ask user which drive they want to recover
-        RecoverDisk = raw_input('\nDisk to recover (defualt is marked in []): ['+color.OKGREEN+'/dev/sda'+color.END+'] ')
+        RecoverDisk = raw_input('\nDisk to recover (defualt is marked in []): ['+color.OKGREEN+'/dev/sda1'+color.END+'] ')
         if RecoverDisk == '':
-                RecoverDisk = '/dev/sda' # defualt choice if input is blank.
+                RecoverDisk = '/dev/sda1' # defualt choice if input is blank.
+	break
 
-# Ask user which drive they want to recover
-        while ((TargetDisk is None) or (not os.path.exists(TargetDisk))):
-		TargetDisk = raw_input(color.FAIL+'Target Drive: '+color.END)
-
-# Search disk for partitions and list them
-# fdisk -l /dev/sda | grep "/dev/" | grep -v Disk
-
-        print "\nUsing Disk {}.\n".format(RecoverDisk)
-	print "\nThe following numbers may be in decimal, hexadecimal or octal, and may be followed by\na multiplier: s = sectors, k = 1000, Ki = 1024, M = 10^6,  Mi  =  2^20, etc"
-	SkipSize = raw_input(color.HEADER+'Skip size?(min,max): '+color.END+'[128] ')
-	if SkipSize == '':
-		SkipSize = '128'
-	ClusterSize = raw_input(color.HEADER+'Cluster size?: '+color.END+'[1024] ')
-        if ClusterSize == '':
-                ClusterSize = '1024'
-	while (CustomerName is None):
-		CustomerName = raw_input(color.FAIL+'Customer Name?: '+color.END+' ')
-	LogFile = "%s_%s.log" % (CustomerName,today.strftime("%m-%d-%y"))
-	LogFile = LogFile.replace(" ","_").replace(",","")
-
-	print "\n"+color.HEADER+"Recovery type:"+color.END+"\nA) Full (runs 3 copy passes, trim, and scrape)\nB) No Scrape (Copy X3, trim)\nC) No Trim (just the copy passes)\nD) Clone (copy pass 1 with a larger read size)\n\nR) Restart\nQ) Quit"
-	# Empty suites are considered syntax errors, so intentional fall-throughs
-	# should contain 'pass'
-	for case in switch(raw_input('Recovery Type []: ')):
-		print "\n\n" # padd down a few lines then print selected options.
-		if case('A'): pass # only necessary if the rest of the suite is empty
-		if case('a'): # Full recovery
-			#
-			_DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose']
-			print "Full Recovery selected."
-			Question = ''
-			break
-	    	if case('B'): pass
-		if case('b'): # No Scrape
-			#
-			_DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose', '--no-scrape']
-			print "No Scrape Recovery selected."
-			Question = ''
-	        	break
-	    	if case('C'): pass
-	    	if case('c'): # No trim
-			#
-			_DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose', '--no-scrape', '--no-trim']
-			print "Full 3 pass clone selected."
-			Question = ''
-	        	break
-		if case('D'): pass
-		if case('d'): # Single forward copy (large block size) good drive clone
-			#
-			_DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--cpass=1', '--idirect', '--odirect', '--force', '--verbose', '--no-trim', '--no-scrape']
-			print "Single pass Clone selected."
-			Question = ''
-			break
-		if case('r'): pass
-		if case('R'): break # user selected R/r for Rerun
-
-		if case('q'): pass
-		if case('Q'):
-			print "Program Terminated"
-			exit(0) #normal program termination
-	    	if case(): # default
-	        	print "Please make a valid selection."
-			# restart the prompts
-
-print "Selected Options."+color.OKBLUE
-print "\n".join(str(x) for x in _DD_OPTIONS_).replace("--", "").replace("="," = ")
-print color.END+color.WARNING+"Executing ddrescue."+color.END
-
-
-# fork the subprocess here
-r, w = os.pipe() # these are file descriptors, not file objects
-
-pid = os.fork()
-if pid:
-    # we are the parent
-    os.close(w) # use os.close() to close a file descriptor
-    r = os.fdopen(r) # turn r into a file object
-    print "Parent: reading"
-    txt = r.read()
-    os.waitpid(pid, 0) # make sure the child process gets cleaned up
-else:
-    # we are the child
-    os.close(r)
-    w = os.fdopen(w, 'w')
-    print "Child: writing"
-    w.write("here's some text from the child")
-    w.close()
-    print "Child: closing"
-    sys.exit(0)
-
-print "Parent: got it; text =", txt
-
-try:
-	print color.OKGREEN+'ddrescue '+RecoverDisk+' '+TargetDisk+' '+LogFile+color.END
-	rescue = Popen(['ddrescue']+_DD_OPTIONS_+[RecoverDisk,TargetDisk,LogFile], stdout=PIPE, stderr=PIPE)
-	out, err = rescue.communicate()
-	print out
-	print color.FAIL+err+color.END
-
-except:
-	print "Error trying to call rescue"
-
-
-# Full recovery will work as follows.
-# 1) Run through on a full copy [cpass 1,2,3] with a larger block size (1024)
-#	Skip size of 128s,1M (128 sectors up to 1 Meg)
-
-# 2) Run a full copy with a smaller block size (128). This is a fast trim copy phase
-#	size of 128s,256s (keep skip size down low to try and copy as much as possible)
-
-# 3) 3rd run no copy, Trim and Scrape Only
