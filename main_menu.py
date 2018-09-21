d51a49ae1a5cd5d1cba6fae2d48b67ff411fb3a2
Levi Modl <hammerzaine@gmail.com>
2018-09-20 19:57:38 -0500

    Added a timer with sleep timer variable

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 ## Include Files Here
+from sys import platform
+import time
 import os
 
 
@@ -20,6 +22,30 @@ class color:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+## OS Specific Stuff
+def GetOS():
+    _SystemOS_ = platform.strip()
+    if (_SystemOS_ == 'linux' or _SystemOS_ == 'linux2'):
+    # linux
+        with open('/etc/os-release') as file:
+            oper = file.readlines()
+            oper = oper[5].split('=')
+            return oper[1].strip() # Grab OS release Name we want to know what flavor of lenny we use.
+    elif(_SystemOS_ == 'win32'):
+        return _SystemOS_
+        
+def OSClear(osname):
+    if(osname == "win32"):
+        os.system("cls")
+    else: # well its not Windows we can just "clear"
+        os.system("clear")
+        
+def OsInput(prompt, os):
+    if(os == "win32"):
+        return input(prompt)
+    elif(os == "debian"):
+        return raw_input(prompt)
+                
 # This class provides the functionality we want. You only need to look at
 # this if you want to know how this works. It only needs to be defined
 # once, no need to muck around with its internals.
@@ -46,40 +72,41 @@ class switch(object):
 
 # Variable definistions
 Question = None
+_OS_ = GetOS()
+_sleep_ = 5
 
 while Question is None:
-	os.system("clear")
+	OSClear(_OS_)
 	colorPrint("Diagnostic and Recovery Programs",color.HEADER)
 	print "(A) GsmartControl - Harddrive Diagnostics."
 	print "(B) Ddrescue - Hard drive recovery + sector cloning."
 	print "(C) Rsync - Backup file systems to drive or server."
-#	print "(D) Chntpw - Offline Windows password reset."
-
+	print "(D) Chntpw - Offline Windows password reset."
+	print "\n----------------------------------------------------------"
 	print "\n(Q) Quit - Closes terminal window"
 	print "(S) Shutdown - Shuts down system"
 	print "(R) Reboot - Reboot system"
+	
 
-
-	for case in switch(raw_input('Select: ')):
-		if case("A"): pass
+	for case in switch(OsInput("Select: ",_OS_).lower()):
 		if case("a"):
 			print("Running gsmart hdd diagnostics.")
+			time.sleep(_sleep_)
 			break
-		if case("B"): pass
 		if case("b"):
 			print("Calling ddrescue.")
+			time.sleep(_sleep_)
 			break
-		if case("C"): pass
 		if case("c"):
 			print("Running rsync backup.")
-		if case("Q"): pass
+			time.sleep(_sleep_)
 		if case("q"):
 			break
-		if case("S"): pass
 		if case("s"):
 			print("Shutting down system")
+			time.sleep(_sleep_)
 			break
-		if case("R"): pass
 		if case("r"):
 			print("Rebooting system")
+			time.sleep(_sleep_)
 			break
