389e6ccdc22b0e51acb8b962f9b0bfbcaee2d45c
Andrew Malone <admmorketh@gmail.com>
2018-09-23 00:44:27 -0500

    Moves menu items into the menu module

--- main_menu.py
+++ main_menu.py
@@ -33,7 +33,6 @@ RecoveryTypeMenu_Items = {'1':["Full", "Copy passes x3, trim, and scrape"],
                           'R':["Restart", "Restarts the Recovery Setup Wizard"],
                           'B':["Back", "Back to main menu"]}
 
-
 # Print Menu method with list of options as input
 class Menu(object):
     _menuformatstring_ = "[{}] {} - {}"
@@ -45,7 +44,27 @@ class Menu(object):
         else:
             self._items_ = _items_
 
+    def Confirm(prompt, option):
+        """Confirm dialog"""
+        confirm = "{}{}{} \"{}\" Are you sure (y/n)?"
+        answer = input(confirm.format(com.color.WARNING, prompt, com.color.END, option)).lower()
+        if answer == "y":
+            return True
+        else:
+            return False
+
+    def GetInputNonEmpty(name):
+        """Refuse empty answers"""
+        option = None
+        while True:
+            option = input("{}{}:{} [ ] ".format(com.color.HEADER,name, com.color.END))
+            if option != "":
+                return option
+            option = None
+            print("{}{} cannot be empty!{}".format(com.color.FAIL,name,com.color.END))
+    
     def Print(self):
+        """Print Multichoice menu"""
         for option, text in self._items_.items():
             if option != "-1":
                 print(self._menuformatstring_.format(option,text[0],text[1]))
@@ -55,40 +74,89 @@ class Menu(object):
 colorPrint("Diagnostic and Recovery Programs",com.color.HEADER)
 MainMenu = Menu(MainMenu_Items)
 
+
 while True:
     MyOS.Clear()
-    
     print("OS Detected: {}".format(MyOS.FormatName()))
-
     MainMenu.Print()
 
-    for case in com.switch(input("Select: ").lower()):
-        if case("1"):
-            print("Running gsmart hdd diagnostics.")
-            time.sleep(_sleep_)
-            break
-        if case("2"):
-            print("Calling ddrescue.")
-            time.sleep(_sleep_)
-            r.rescue()
-            break
-        if case("3"):
-            print("Running rsync backup.")
-            time.sleep(_sleep_)
-            break
-        if case("4"):
-            print("Starting Offline Regedit.")
-            time.sleep(_sleep_)
-            break
-        if case("q"):
-            sys.exit()
-        if case("s"):
-            print("Shutting down system")
-            time.sleep(_sleep_)
-            MyOS.Shutdown()
-            break
-        if case("r"):
-            print("Rebooting system")
-            time.sleep(_sleep_)
-            MyOS.Shutdown()
-            break
+    case = MenuInput()
+
+
+def rescue():
+# Clear screen
+        
+        os.system("clear")
+        Task = Recovery()
+        
+        TaskOptions = Task.GetConfigFromUser()
+        TaskOptions['LogFile'] = Task.GetLogName()
+
+        print("\n"+com.color.HEADER+"Recovery type:"+com.color.END+"\nA) Full (runs 3 copy passes, trim, and scrape)\nB) No Scrape (Copy X3, trim)\nC) No Trim (just the copy passes)\nD) Clone (copy pass 1 with a larger read size)\n\nR) Restart\nQ) Quit")
+
+        # Empty suites are considered syntax errors, so intentional fall-throughs
+	    # should contain 'pass'
+        for case in com.switch(input('Recovery Type []: ').lower()):
+            print("\n\n") # padd down a few lines then print selected options.
+            if case('a'): # Full recovery
+			#
+                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose']
+                print("Full Recovery selected.")
+                Question = ''
+                break
+            if case('b'): # No Scrape
+			#
+                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose', '--no-scrape']
+                print("No Scrape Recovery selected.")
+                Question = ''
+                break
+            if case('c'): # No trim
+			#
+                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose', '--no-scrape', '--no-trim']
+                print("Full 3 pass clone selected.")
+                Question = ''
+                break
+            if case('d'): # Single forward copy (large block size) good drive clone
+			#
+                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--cpass=1', '--idirect', '--odirect', '--force', '--verbose', '--no-trim', '--no-scrape']
+                print("Single pass Clone selected.")
+                Question = ''
+                break
+            if case('r'): break
+
+            if case('q'):
+                print("Program Terminated")
+                exit(0) #normal program termination
+
+            if case(): # default
+                print("Please make a valid selection.")
+			# restart the prompts
+
+        print("Selected Options."+com.color.OKBLUE)
+        print("\n".join(str(x) for x in _DD_OPTIONS_).replace("--", "").replace("="," = "))
+        print(com.color.END+com.color.WARNING+"Executing ddrescue."+com.color.END)
+
+        # fork the subprocess here
+        r, w = os.pipe() # these are file descriptors, not file objects
+
+        pid = os.fork()
+        if pid:
+        # we are the parent
+            os.close(w) # use os.close() to close a file descriptor
+            print(com.color.OKGREEN+'ddrescue '+RecoverDisk+' '+TargetDisk+' '+LogFile+com.color.END)
+            DoRescue(LogFile, RecoverDisk, TargetDisk, _DD_OPTIONS_)
+            os.waitpid(pid, 0) # make sure the child process gets cleaned up
+        else:
+        # we are the child
+            os.close(r)
+            Popen(['ddrescueview', RescueLogPath+"/"+LogFile], stdout=PIPE, stderr=PIPE)
+            sys.exit(0)
+
+# Full recovery will work as follows.
+# 1) Run through on a full copy [cpass 1,2,3] with a larger block size (1024)
+#	Skip size of 128s,1M (128 sectors up to 1 Meg)
+
+# 2) Run a full copy with a smaller block size (128). This is a fast trim copy phase
+#	size of 128s,256s (keep skip size down low to try and copy as much as possible)
+
+# 3) 3rd run no copy, Trim and Scrape Only
--- rescue.py
+++ rescue.py
@@ -23,16 +23,6 @@ NtfsFix = ['ntfsfix', '--clear-bad-sectors', '--clean-dirty']
 MkDir = ['mkdir','-p']
 
 
-def doMount(): # Needs refactoring (move to class Recovery()
-    try:
-        print("Mounting Storage Server....")
-        err = check_output(RescueMount)
-        print(com.color.OKGREEN+"Server Drive Mounted."+com.color.END)
-        time.sleep(10)
-    except CalledProcessError as ERROR:
-        print(com.color.FAIL+"ERROR while mounting "+RescueMount[3]+'\nReturned with Error:\n>>>> '+str(ERROR)+com.color.END)
-        exit(ERROR.returncode)
-
 class Recovery(object):
     """Defines a Recovery Environment Object"""
     _today_ = date.today().strftime("%m-%d-%y")
@@ -52,6 +42,16 @@ class Recovery(object):
               'LogPath':'/media/data/DDRescue_Logs',
               'LogFile':''
               }
+
+    def doMount(): # Needs refactoring (move to class Recovery()
+        try:
+            print("Mounting Storage Server....")
+            err = check_output(RescueMount)
+            print(com.color.OKGREEN+"Server Drive Mounted."+com.color.END)
+            time.sleep(10)
+        except CalledProcessError as ERROR:
+            print(com.color.FAIL+"ERROR while mounting "+RescueMount[3]+'\nReturned with Error:\n>>>> '+str(ERROR)+com.color.END)
+            exit(ERROR.returncode)
 
     def __init__(self, *args, **kwargs):
         return super().__init__(*args, **kwargs)
@@ -76,27 +76,9 @@ class Recovery(object):
         """Saves the user settings"""
         self._config_ = _newConfig_
 
-    def GetInputNonEmpty(self, name):
-        option = None
-        while True:
-            option = input("{}{}:{} [ ] ".format(com.color.HEADER,name, com.color.END))
-            if option != "":
-                return option
-            option = None
-            print("{}{} cannot be empty!{}".format(com.color.FAIL,name,com.color.END))
-
-    def Confirm(self, prompt, option):
-        confirm = "{}{}{} \"{}\" Are you sure (y/n)?"
-        answer = ""
-        answer = input(confirm.format(com.color.WARNING, prompt, com.color.END, option)).lower()
-        if answer == "y":
-            return True
-        else:
-            return False
-
     def GetConfigFromUser(self):
         """Gets data from user to define recovery environment"""
-        
+
         UserOptions = self._config_
 
         UserOptions['RecoveryDisk'] = input("{}Disk to recover (defualt marked in []):{} [ {} ] ".format(com.color.HEADER, com.color.END,self._GetConfig('RecoveryDisk')))
@@ -106,7 +88,7 @@ class Recovery(object):
         UserOptions['skip-size'] = input("{}Skip Size (min,max):{} [ {} ] ".format(com.color.HEADER, com.color.END,self._GetConfig('skip-size')))
         UserOptions['cluster-size'] = input("{}Cluster Size:{} [ {} ] ".format(com.color.HEADER, com.color.END,self._GetConfig('cluster-size')))
 
-        
+
         UserOptions['TargetDisk'] = self.GetInputNonEmpty("Target Disk")
 
         while not self.Confirm("Target Disk", UserOptions['TargetDisk']):
@@ -151,84 +133,3 @@ class Recovery(object):
     
         except (ValueError, KeyError, TypeError):
             print("lsblk returned the wrong JSON format")
-
-
-def rescue():
-# Clear screen
-        
-        os.system("clear")
-        Task = Recovery()
-        
-        TaskOptions = Task.GetConfigFromUser()
-        TaskOptions['LogFile'] = Task.GetLogName()
-        
-
-
-        print("\n"+com.color.HEADER+"Recovery type:"+com.color.END+"\nA) Full (runs 3 copy passes, trim, and scrape)\nB) No Scrape (Copy X3, trim)\nC) No Trim (just the copy passes)\nD) Clone (copy pass 1 with a larger read size)\n\nR) Restart\nQ) Quit")
-	# Empty suites are considered syntax errors, so intentional fall-throughs
-	# should contain 'pass'
-        for case in com.switch(input('Recovery Type []: ').lower()):
-            print("\n\n") # padd down a few lines then print selected options.
-            if case('a'): # Full recovery
-			#
-                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose']
-                print("Full Recovery selected.")
-                Question = ''
-                break
-            if case('b'): # No Scrape
-			#
-                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose', '--no-scrape']
-                print("No Scrape Recovery selected.")
-                Question = ''
-                break
-            if case('c'): # No trim
-			#
-                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--skip-size='+SkipSize, '--reopen-on-error', '--idirect', '--odirect', '--force', '--verbose', '--no-scrape', '--no-trim']
-                print("Full 3 pass clone selected.")
-                Question = ''
-                break
-            if case('d'): # Single forward copy (large block size) good drive clone
-			#
-                _DD_OPTIONS_ = ['--cluster-size='+ClusterSize, '--cpass=1', '--idirect', '--odirect', '--force', '--verbose', '--no-trim', '--no-scrape']
-                print("Single pass Clone selected.")
-                Question = ''
-                break
-            if case('r'): break
-
-            if case('q'):
-                print("Program Terminated")
-                exit(0) #normal program termination
-
-            if case(): # default
-                print("Please make a valid selection.")
-			# restart the prompts
-
-        print("Selected Options."+com.color.OKBLUE)
-        print("\n".join(str(x) for x in _DD_OPTIONS_).replace("--", "").replace("="," = "))
-        print(com.color.END+com.color.WARNING+"Executing ddrescue."+com.color.END)
-
-
-        # fork the subprocess here
-        r, w = os.pipe() # these are file descriptors, not file objects
-
-        pid = os.fork()
-        if pid:
-        # we are the parent
-            os.close(w) # use os.close() to close a file descriptor
-            print(com.color.OKGREEN+'ddrescue '+RecoverDisk+' '+TargetDisk+' '+LogFile+com.color.END)
-            DoRescue(LogFile, RecoverDisk, TargetDisk, _DD_OPTIONS_)
-            os.waitpid(pid, 0) # make sure the child process gets cleaned up
-        else:
-        # we are the child
-            os.close(r)
-            Popen(['ddrescueview', RescueLogPath+"/"+LogFile], stdout=PIPE, stderr=PIPE)
-            sys.exit(0)
-
-# Full recovery will work as follows.
-# 1) Run through on a full copy [cpass 1,2,3] with a larger block size (1024)
-#	Skip size of 128s,1M (128 sectors up to 1 Meg)
-
-# 2) Run a full copy with a smaller block size (128). This is a fast trim copy phase
-#	size of 128s,256s (keep skip size down low to try and copy as much as possible)
-
-# 3) 3rd run no copy, Trim and Scrape Only
