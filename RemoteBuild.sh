cd3c6aac8d4a7d098653e13df8adbb1afce0cbd9
Andrew Malone <admmorketh@gmail.com>
2019-11-27 03:17:54 -0700

    adds in mounting and copping of iso/squashfs

--- RemoteBuild.sh
+++ RemoteBuild.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-DOWNLOAD_PATH=/mnt/d/PxeTools/ISO
+DOWNLOAD_PATH=/media/ISO-Storage
+TMP_Mount=/media/iso-mntpoint
+ISO_NAME=Kali-CI-$(date +"%m-%d-%Y").iso
 DEVTermIP=192.168.1.24
 DEVUser=root
 DEVTermSSHPort=22
 
 cd $DOWNLOAD_PATH || echo "Path or directory does not exist /!\\"; exit
 
-rm -v kali-linux-rolling-CI-amd64.iso
-
 ssh $DEVUser@$DEVTermIP -p $DEVTermSSHPort 'service apache2 start; cd ~/repos/DRS; git pull; bash cw_kali_conf.sh'
 
-wget http://$DEVTermIP/kali-linux-rolling-CI-amd64.iso
+wget -O $ISO_NAME http://$DEVTermIP/kali-linux-rolling-CI-amd64.iso
 
-#TODO Add unpacking and copy for TFTP server location.
+mount -o loop $ISO_NAME $TMP_Mount
+
+cp $TMP_Mount/live/filesystem.squashfs /media/SquashFS/kali-CI.squashfs
+
+
+# DirectoryTree = {Apache-webshare,TFTP-Boot,FTP-share,SMB-share,NFS-Config,ISO-Storage,SquashFS}
+
+# TFTP-Root
+#		|
+#		|
+#	[SquashFS]
+#			|
+#		(distro.squashfs)
